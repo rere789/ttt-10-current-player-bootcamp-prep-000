5e08736470eb6d546bebbd5d472cab8afe2ea5ed
Reina Ewing <reina.castro3@gmail.com>
2019-05-03 14:59:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,20 +4,13 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " " ]
 
 def turn_count(board_array)
   counter = 0 
-  if board_array[] == "X"
-    puts "It's O turn"
-  else 
-    puts "It's X turn "
-  
-  
-  
-  # board_array.each do |turn|
-  #   if board_array == "X" 
-  #     puts "It's O turn"
-  #   elsif
-  #     board_array == "O"
-  #     puts "It's X turn"
-  #     counter += 1 
-  # end
+  board_array.each do |turn|
+    if board_array == "X" 
+      puts "It's O turn"
+    elsif
+      board_array == "O"
+      puts "It's X turn"
+      counter += 1 
+  end
 end 
 end
