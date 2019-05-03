e04c70a75de93cff99a8a796b10d3632d59bcca6
Reina Ewing <reina.castro3@gmail.com>
2019-05-03 14:55:13 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " " ]
+
+
+
+def turn_count(board_array)
+  counter = 0 
+  board_array.each do |turn|
+    if board_array == "X" 
+      puts "It's O turn"
+    elsif
+      board_array == "O"
+      puts "It's X turn"
+      counter += 1 
+  end
+end 
+end
