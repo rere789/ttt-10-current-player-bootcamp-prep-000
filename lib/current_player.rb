fe37d999107faae191aaef306c52232e87d858bb
Reina Ewing <reina.castro3@gmail.com>
2019-05-03 15:21:47 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,8 +12,8 @@ def turn_count(turns)
 end 
 
 def current_player(board)
-    num = turn_count(board) 
-   if num.even?
+    # num = turn_count(board) 
+   if turn_count(board).even?
       return "X"
     else
       return "O"
