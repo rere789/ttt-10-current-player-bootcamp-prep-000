6d0fb5461d133c9ea6927263042ceffa34a026ac
Reina Ewing <reina.castro3@gmail.com>
2019-05-03 15:11:05 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,29 +1,12 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " " ]
 
 
-
-# def turn_count(turn)
-#   counter = [] 
-#   turn.each do |turns|
-#     if turns == "X" || turns == "O"
-#       counter += 1 
-#   end
-# end 
-#   counter.length.to_i
-# end
-
-def turn_count(turn)
-  #counter = 1
-  move_count = []
-  turn.each do |unit|
-    #counter = 0
-    if unit == "X" || unit == "O"
-      #puts "spot taken"
-      move_count.push(1)
-    #else 
-      #puts "spot free"
-      #counter += 1
-    end 
+def turn_count(turns)
+  counter = []
+  turns.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1 
+    end
   end 
-  move_count.length.to_i
+  counter.length.to_i 
 end 
